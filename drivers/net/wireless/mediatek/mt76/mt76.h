f7910cefdc2dee595ab1e159316dcbc112aea890
Felix Fietkau <nbd@nbd.name>
2022-07-03 20:59:05 +0200

    mt76: remove q->qid
    It is no longer used
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit bceb8b8ded9b2470c10a13e5446f2d5a8e178964)

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -186,7 +186,6 @@ struct mt76_queue {
 
 	u8 buf_offset;
 	u8 hw_idx;
-	u8 qid;
 	u8 flags;
 
 	u32 wed_regs;
@@ -978,7 +977,6 @@ static inline int mt76_init_tx_queue(struct mt76_phy *phy, int qid, int idx,
 	if (IS_ERR(q))
 		return PTR_ERR(q);
 
-	q->qid = qid;
 	phy->q_tx[qid] = q;
 
 	return 0;
@@ -993,7 +991,6 @@ static inline int mt76_init_mcu_queue(struct mt76_dev *dev, int qid, int idx,
 	if (IS_ERR(q))
 		return PTR_ERR(q);
 
-	q->qid = __MT_TXQ_MAX + qid;
 	dev->q_mcu[qid] = q;
 
 	return 0;
