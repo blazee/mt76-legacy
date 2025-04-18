8cefacd8abf5325e31c2724c548f1a7181b0b44a
blazee <blazee.dev@gmail.com>
2025-04-18 22:27:40 +0200

    bp: all: Add new *_PM_OPS macros for 5.16

--- drivers/net/wireless/mediatek/mt76/backport/compat.h
+++ drivers/net/wireless/mediatek/mt76/backport/compat.h
@@ -7,3 +7,25 @@
 #ifndef IEEE80211_MAX_AMPDU_BUF_HE
 #define IEEE80211_MAX_AMPDU_BUF_HE 0x100
 #endif
+
+// 75d4f92d8788e7df3ff864e0ba42bb4d1bcd6eda git into 5.15 but not .16
+#ifndef SYSTEM_SLEEP_PM_OPS
+#define SYSTEM_SLEEP_PM_OPS(suspend_fn, resume_fn) \
+	.suspend = pm_sleep_ptr(suspend_fn), \
+	.resume = pm_sleep_ptr(resume_fn), \
+	.freeze = pm_sleep_ptr(suspend_fn), \
+	.thaw = pm_sleep_ptr(resume_fn), \
+	.poweroff = pm_sleep_ptr(suspend_fn), \
+	.restore = pm_sleep_ptr(resume_fn),
+#endif
+
+#ifndef DEFINE_SIMPLE_DEV_PM_OPS
+#define DEFINE_SIMPLE_DEV_PM_OPS(name, suspend_fn, resume_fn) \
+const struct dev_pm_ops name = { \
+	SYSTEM_SLEEP_PM_OPS(suspend_fn, resume_fn) \
+}
+#endif
+
+#ifndef pm_sleep_ptr
+#define pm_sleep_ptr(_ptr) PTR_IF(IS_ENABLED(CONFIG_PM_SLEEP), (_ptr))
+#endif
